f32e101e5a3065e0db5a4d30b1454adff6327c73
nick <nicholasdipreta2020@gmail.com>
2020-06-04 19:06:16 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+if (typeof $ == 'undefined'){
+    console.log('oops! I still have to link my jQuery properly!');
+  } else {console.log('I did it! I linked jQuery and this js file properly!')};
+
+const $name = $('h1');
+const $prompt = $('p');
+const $entry = $('.entry')
+    $name.click(function(){
+      $name.animate({height: '100px', opacity: '0.4'}, "slow");
+      $prompt.toggle();
+      $entry.toggle();
+    });
+  
+    $('.input').keypress(function (e) {
+      if (e.which == 13) {
+        $('form#login').submit();
+        return false;    //<---- Add this line
+      }
+    });
+
+$("#welcome").fadeIn(3000);
+
+
+  });
